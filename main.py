837de39ed1001e3a892fc7d88f9e86200a3764b6
NF-100038730 <nathan.a.fernald@gmail.com>
2024-02-18 00:32:29 -0600

    Experimenting with navigation of websites

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 import time
 
 #path = "C:/Users/Nathan/Desktop/chromedriver-win64.zip"
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://www.ccisd.net/")
+driver.get("https://www.ticketmaster.com/")
 
-while(True):
-    pass
+running = True
+
+while(running):
+    search = driver.find_element(By.ID, 'searchFormInput-input')
+    search.send_keys("Taylor Swift")
+    search.send_keys(Keys.RETURN)
+    time.sleep(10)
+
+    running = False
+
+else:
+    driver.quit()
