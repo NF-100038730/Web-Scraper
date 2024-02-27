366b7c9499854645d8ff76d47af073af337575ff
NF-100038730 <nathan.a.fernald@gmail.com>
2024-02-27 00:37:10 -0600

    Working on errors in regards to StubHub soup not beingn created

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 artist = input("Enter artist (eg. taylor swift) >> ")
-month = input("Enter month (eg. Oct) >> ")
+month = input("Enter month (eg. 10 for Oct) >> ")
 
 artist_p20 = ""
 artist_d = ""
@@ -16,27 +16,29 @@ for c in artist:
         artist_d += c
 
 tm_url = f"https://www.ticketmaster.com/search?q={artist_p20}"
-sh_url = f"https://www.stubhub.com/secure/search?q={artist_p20}"
+sh_url = f"https://www.stubhub.com/secure/search?q={artist_p20}&sellSearch=false"
 
-#print(tm_url)
-#print(sh_url)
+tm_prelim_response = requests.get(tm_url)
+tm_prelim_soup = BeautifulSoup(tm_prelim_response.text, "html.parser")
 
-tm_response = requests.get(tm_url)
-tm_soup = BeautifulSoup(tm_response.text, "html.parser")
-
-sh_response = requests.get(sh_url)
-sh_soup = BeautifulSoup(sh_response.text, "html.parser")
-
-for links in tm_soup.find_all("a"):
+for links in tm_prelim_soup.find_all("a"):
     if artist_d in links.get("href"):
         tm_url = "https://www.ticketmaster.com" + links.get("href")
         break
 
-print(tm_url)
-    
-for links in sh_soup.find_all("a"):
-    if artist_d in links.get("href"):
-        sh_url = links.get("href")
-        break
+tm_response = requests.get(tm_url)
+tm_soup = BeautifulSoup(tm_response.text, "html.parser")
 
-print(sh_url)
+# dates = tm_soup.find_all(string=month)
+# parent = dates[0].parent
+# print(parent)
+
+# sh_response = requests.get(sh_url)
+# sh_soup = BeautifulSoup(sh_response.content, "html.parser")
+    
+# for links in sh_soup.find_all("a"):
+#     if artist_d in links.get("href"):
+#         sh_url = links.get("href")
+#         break
+
+# print(sh_url)
