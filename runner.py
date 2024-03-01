e78a151c7e9a89f20264977e4305a94b51b75196
NF-100038730 <nathan.a.fernald@gmail.com>
2024-03-01 16:20:56 -0600

    creating functions to get html syntax of entered values

--- runner.py
+++ runner.py
@@ -1,11 +1,11 @@
 from argparse import ArgumentParser, Namespace
-
 from main import *
 
 parser = ArgumentParser()
 parser.usage = "Ticketmaster Web Scraper"
 parser.add_argument("--state", type=str, help="str >> State that contains the wanted city (e.g. TX)")
-parser.add_argument("--city", type=str, help="str >> City that will be price checked (e.g. College Station)")
+parser.add_argument("--city", type=str, help="str >> City that will be price checked (e.g. 'College Station')")
+parser.add_argument("--fuel", type=str, help="str >> Type of fuel (Regular, Midgrade, Premium, and Diesel)")
 args: Namespace = parser.parse_args()
 
-check_state(args.state, args.city)
+check_state(args.state, args.city, args.fuel)
