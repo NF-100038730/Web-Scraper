964cac286b117ed4477c1b69a5aad2893b81889e
NF-100038730 <nathan.a.fernald@gmail.com>
2024-03-07 22:41:48 -0600

    Update usage in parser

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ from argparse import ArgumentParser, Namespace
 from main import *
 
 parser = ArgumentParser()
-parser.usage = "Ticketmaster Web Scraper"
+parser.usage = "GasBuddy Web Scraper"
 parser.add_argument("--state", type=str, help="str >> State that contains the wanted city (e.g. TX)")
 parser.add_argument("--city", type=str, help="str >> City that will be price checked (e.g. 'College Station')")
 parser.add_argument("--fuel", type=str, help="str >> Type of fuel (Regular:default, Midgrade, Premium, and Diesel)", default="Regular")
